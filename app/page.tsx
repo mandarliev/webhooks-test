d7afe2e7b40e065c872cef33b39481cc2194b93d
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-28 21:24:49 +0200

    cleaning stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Webhooks</h1>
+      <h1 className="text-xl text-indigo-500">Welcome to a Webhooks lesson!</h1>
     </main>
   );
 }
